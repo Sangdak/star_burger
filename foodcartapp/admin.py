208a820d22b8e7b91384bcf28abfbbe1d1304e5f
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-08-22 04:09:02 +1000

    Made custom redirect from order edit page.

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, HttpResponseRedirect
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -119,4 +121,9 @@ class OrderAdmin(admin.ModelAdmin):
         OrderItemInline,
     ]
 
-
+    def response_change(self, request, obj):
+        custom_response = super(OrderAdmin, self).response_change(request, obj)
+        if 'next' in request.GET and url_has_allowed_host_and_scheme(request.GET['next'], settings.ALLOWED_HOSTS):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return custom_response
